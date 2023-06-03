be0e8202d5296b826f678ad53a22c308fcfacf89
Rishabh Anand <ranand16@gmail.com>
2023-06-03 11:52:12 +0530

    Update to the redis configuration

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -1,6 +1,9 @@
 import { Redis } from "@upstash/redis";
 
-export const redisdb = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
+// export const redisdb = new Redis({
+//   url: process.env.UPSTASH_REDIS_REST_URL,
+//   token: process.env.UPSTASH_REDIS_REST_TOKEN,
+// });
+
+// The above commented code is same as the below line
+export const redisdb = Redis.fromEnv();
